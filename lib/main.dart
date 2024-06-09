ab433bb023bcafcd37b2b0278571cc9d59a2ea8a
suriyaa07 <suryansprof2004@gmail.com>
2024-06-09 22:28:58 +0530

    started login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
+import 'package:backendex/pages/login_page.dart';
 import 'package:flutter/material.dart';
-import 'package:backendex/pages/homepage.dart';
+//import 'package:backendex/pages/homepage.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: LoginPage(),
       theme: ThemeData(primarySwatch: Colors.red),
     ),
   );
